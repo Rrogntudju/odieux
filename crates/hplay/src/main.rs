410f3002551843883ac098a62d1549c5c86be6d6
Rrogntudju <rrogntudju@example.com>
2021-09-22 11:36:49 -0400

    wip

--- crates/hplay/src/main.rs
+++ crates/hplay/src/main.rs
@@ -10,14 +10,16 @@ let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 let sink = Sink::try_new(&stream_handle).unwrap();
 
 // Add a dummy source of the sake of the example.
-let source1 = SineWave::new(440).take_duration(Duration::from_secs_f32(0.50)).amplify(0.20);
+let source1 = SineWave::new(440).take_duration(Duration::from_secs_f32(1.0)).amplify(0.20);
 let source2 = SineWave::new(440).take_duration(Duration::from_secs_f32(0.50)).amplify(0.20);
 let file = BufReader::new(File::open(r"C:\Users\Scotty\Downloads\sample3.aac").unwrap());
 // Decode that sound file into a source
 let source3 = Decoder::new(file).unwrap();
 sink.append(source1);
+std::thread::sleep(Duration::from_secs_f32(0.50));
 sink.append(source2);
-sink.append(source3);
+std::thread::sleep(Duration::from_secs_f32(0.50));
+sink.append(source3); 
 
 // The sound plays in a separate thread. This call will block the current thread until the sink
 // has finished playing all its queued sounds.
