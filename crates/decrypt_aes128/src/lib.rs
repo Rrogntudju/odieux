2b6d2ab3c463caaf15277f068281eb9c4b3a3eb9
Rrogntudju <rrogntudju@example.com>
2021-09-23 13:12:06 -0400

    AES-128 CBC

---
+++ crates/decrypt_aes128/src/lib.rs
@@ -0,0 +1,23 @@
+use aes::Aes128;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use anyhow::{Result, Context};
+
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+
+pub fn decrypt_aes128(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>> {
+    let mut encrypted_data = data.to_owned();
+    let cipher = Aes128Cbc::new_from_slices(&key, &iv).context("Échec de la création du chiffre AES-128 CBC")?;
+    Ok(cipher.decrypt(&mut encrypted_data).context("Échec de la décryption")?.to_vec())
+}
+
+#[test]
+fn test_demo_decrypt() {
+    let key = "4567890123456789".as_bytes();
+    let iv = "1234567890123456".as_bytes();
+    let data = [0xDA, 0x52, 0xF9, 0x7B, 0xAB, 0xAE, 0x0A, 0x79, 0x7F, 0x1C, 0x11, 0xEC, 0xB2, 0x09, 0x9F, 0xB0];
+
+    let result = decrypt_aes128(&key, &iv, &data).unwrap();
+    assert_eq!(String::from_utf8(result).unwrap(), "DOH!");
+}
--- crates/hls_player/src/hls_handler.rs
+++ crates/hls_player/src/hls_handler.rs
@@ -1,11 +1,27 @@
 use hls_m3u8::{MasterPlaylist, MediaPlaylist,};
 use mpeg2ts::ts::payload::Bytes;
 use std::collections::VecDeque;
-use url;
+use std::convert::TryFrom;
+use url::Url;
 use anyhow::{Result, Context, Error};
+use std::sync::mpsc::{Receiver, SyncSender, sync_channel};
+use std::thread;
 
-struct HlsHandler<'a> {
-    masterPlayList: MasterPlaylist<'a>,
-    mediaPlayList: MediaPlaylist<'a>,
-    streams: VecDeque<Bytes>,
+struct HlsHandler {
+    // masterPlayList: MasterPlaylist<'a>,
+    // mediaPlayList: MediaPlaylist<'a>,
+   // streams: VecDeque<Bytes>,
+   sender: Option<SyncSender<Box<Vec<u8>>>>
+}
+
+impl HlsHandler {
+    fn new() -> Self {
+
+        HlsHandler { sender: None }
+    }
+
+    fn start(&mut self, url: &str) -> Result<Receiver<Box<Vec<u8>>> {
+        let master_url = Url::try_from(url)?.
+
+    }
 }
