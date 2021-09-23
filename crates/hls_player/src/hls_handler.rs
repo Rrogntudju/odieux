ec420cededa291235774275e643d8f23c5f596b4
Rrogntudju <rrogntudju@example.com>
2021-09-23 18:41:01 -0400

    start

--- crates/hls_player/src/hls_handler.rs
+++ crates/hls_player/src/hls_handler.rs
@@ -1,27 +1,31 @@
 use hls_m3u8::{MasterPlaylist, MediaPlaylist,};
 use mpeg2ts::ts::payload::Bytes;
-use std::collections::VecDeque;
 use std::convert::TryFrom;
 use url::Url;
-use anyhow::{Result, Context, Error};
+use anyhow::{Result, Context};
 use std::sync::mpsc::{Receiver, SyncSender, sync_channel};
 use std::thread;
+use decrypt_aes128::decrypt_aes128;
+
+ 
+fn handle_hls(url: Url, tx: SyncSender<Box<Vec<u8>>>) {
+
+}
 
 struct HlsHandler {
-    // masterPlayList: MasterPlaylist<'a>,
-    // mediaPlayList: MediaPlaylist<'a>,
-   // streams: VecDeque<Bytes>,
    sender: Option<SyncSender<Box<Vec<u8>>>>
 }
 
 impl HlsHandler {
     fn new() -> Self {
-
         HlsHandler { sender: None }
     }
 
-    fn start(&mut self, url: &str) -> Result<Receiver<Box<Vec<u8>>> {
-        let master_url = Url::try_from(url)?.
+    fn start(&mut self, url: &str) -> Result<Receiver<Box<Vec<u8>>>> {
+        let master_url = Url::try_from(url).context("Validation de l'url MasterPlaylist")?;
+        let (tx, rx) = sync_channel::<Box<Vec<u8>>>(2);
+        thread::spawn(move || handle_hls(master_url, tx));
 
+        Ok(rx)
     }
 }
