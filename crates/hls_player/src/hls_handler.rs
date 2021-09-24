25da02e6c4500215290b0342d1933090cb299664
Rrogntudju <rrogntudju@example.com>
2021-09-24 15:36:31 -0400

    wip

--- crates/hls_player/src/hls_handler.rs
+++ crates/hls_player/src/hls_handler.rs
@@ -9,29 +9,32 @@ use std::thread;
 use url::Url;
 
 type Message = Result<Box<Vec<u8>>>;
-const TIME_OUT: u64 = 10; 
+const TIME_OUT: u64 = 10;
+const BOUND: usize = 2;
 
 fn handle_hls(url: Url, tx: SyncSender<Message>) {
-    let response = match minreq::get(url).with_timeout(TIME_OUT).send() {
+    let response = match minreq::get(url).with_timeout(TIME_OUT).send().context("Téléchargement de MasterPlayList") {
         Ok(response) => response,
         Err(e) => {
-            tx.send(Err(anyhow!(e))).unwrap_or_default();
+            tx.send(Err(e)).unwrap_or_default();
             return;
         }
     };
-    
-    let masterPl = match MasterPlaylist::try_from(response.as_str().unwrap_or_default()) {
+
+    let masterPl = match MasterPlaylist::try_from(response.as_str().unwrap_or_default()).context("Validation de MasterPlayList") {
         Ok(mpl) => mpl,
         Err(e) => {
-            tx.send(Err(anyhow!(e))).unwrap_or_default();
+            tx.send(Err(e)).unwrap_or_default();
             return;
         }
     };
+
+    let vs = masterPl.audio_streams().max_by_key(|vs| vs.bandwidth());  // Select stream with maximum bitrate
 }
 
 pub fn start(url: &str) -> Result<Receiver<Message>> {
     let master_url = Url::try_from(url).context("Validation de l'url MasterPlaylist")?;
-    let (tx, rx) = sync_channel::<Message>(2);
+    let (tx, rx) = sync_channel::<Message>(BOUND);
     thread::spawn(move || handle_hls(master_url, tx));
 
     Ok(rx)
