1e84ae385b8a105e78e74155902dac7106aacf9c
Rrogntudju <rrogntudju@example.com>
2021-09-29 17:09:53 -0400

    wip

--- crates/hls_player/src/lib.rs
+++ crates/hls_player/src/lib.rs
@@ -1,10 +1,25 @@
+use std::sync::mpsc::Receiver;
 use anyhow::{anyhow, Context, Result};
-use hls_handler::start;
+use hls_handler::{start, Message};
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+enum State {
+    Started,
+    Stopped,
+    Paused,
+    Playing,
+}
+
+pub struct Player {
+    rx : Option<Receiver<Message>>,
+    state: State,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self { rx: None, state : State::Stopped}
+    }
+
+    pub fn start(url: &str) -> Result<Result<Receiver<Message>>> {
+        
     }
 }
