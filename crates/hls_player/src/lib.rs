76babfd2c88c95b21d914974cf17941457b93395
Rrogntudju <rrogntudju@example.com>
2021-09-30 17:33:22 -0400

    wip

--- crates/hls_player/src/lib.rs
+++ crates/hls_player/src/lib.rs
@@ -1,14 +1,13 @@
 use std::sync::{Arc, Mutex};
 use anyhow::{anyhow, Context, Result};
 use hls_handler::{start, Message};
-use rodio::Sink;
+use rodio::{OutputStream, Sink};
 
 
 enum State {
     Started,
     Stopped,
     Paused,
-    Playing,
 }
 
 pub struct Player {
@@ -23,7 +22,13 @@ impl Player {
     }
 
     pub fn start(&mut self, url: &str) -> Result<()> {
+        let tx = hls_handler::start(url)?;
+        let (_, stream_handle) = OutputStream::try_default()?;
+        let sink = Arc::new(Mutex::new(Sink::try_new(&stream_handle)?));
+        let sink2 = sink.clone();
+        self.sink = Some(sink);
 
+        Ok(())
     }
 
     pub fn play(&mut self)  {
