6820aef90f3714c5003011f054be0d3b1554c037
Rrogntudju <rrogntudju@example.com>
2021-10-05 15:22:25 -0400

    hplay

--- crates/hplay/src/main.rs
+++ crates/hplay/src/main.rs
@@ -1,26 +1,19 @@
-fn main() {
-    use std::fs::File;
-    use std::io::BufReader;
-    use std::time::Duration;
-    use rodio::{Decoder, OutputStream, Sink};
-    use rodio::source::{SineWave, Source};
+use hls_player::Player;
+use std::env::args;
+use std::error::Error;
+use std::io::stdin;
 
-    let (_output_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let arg = match args().nth(1) {
+        Some(arg) => arg,
+        None => return Err("Fournir un url master.m3u8 validé".into()),
+    };
 
-    // Add a dummy source of the sake of the example.
-    let source1 = SineWave::new(440).take_duration(Duration::from_secs_f32(1.0)).amplify(0.20);
-    let source2 = SineWave::new(440).take_duration(Duration::from_secs_f32(0.50)).amplify(0.20);
-    let file = BufReader::new(File::open(r"C:\Users\Scotty\Downloads\sample3.aac").unwrap());
-    // Decode that sound file into a source
-    let source3 = Decoder::new(file).unwrap();
-    sink.append(source1);
-    std::thread::sleep(Duration::from_secs_f32(0.50));
-    sink.append(source2);
-    std::thread::sleep(Duration::from_secs_f32(0.50));
-    sink.append(source3); 
+    let mut player = Player::start(&arg)?;
 
-    // The sound plays in a separate thread. This call will block the current thread until the sink
-    // has finished playing all its queued sounds.
-    sink.sleep_until_end();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap_or_default();
+    player.stop();
+
+    Ok(())
 }
