1b49a99794ea5daf2a1fcf69ff2cec511c9422ef
Rrogntudju <rrogntudju@example.com>
2021-10-14 17:39:11 -0400

    wip

--- crates/gratte/src/main.rs
+++ crates/gratte/src/main.rs
@@ -1,21 +1,21 @@
 use serde::Serialize;
+use soup::prelude::*;
 use std::default::Default;
 use std::env;
 use std::error::Error;
 use std::fs;
-use soup::Soup;
 
 #[derive(Serialize)]
-struct Emission {
+struct Episode {
     titre: String,
-    url: String,
+    media_id: String,
 }
 
 #[derive(Serialize, Default)]
-struct Emissions(Vec<Emission>);
+struct Episodes(Vec<Episode>);
 
 fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
-    let mut émissions = Emissions::default();
+    let mut épisodes = Episodes::default();
     for i in 1.. {
         let response = match minreq::get(url).with_timeout(10).send() {
             Ok(response) => match response.status_code {
@@ -26,11 +26,25 @@ fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
             Err(e) => return Err(e.into()),
         };
         let soup = Soup::new(response.as_str().unwrap_or("DOH!"));
+
+        let script = soup
+            .tag("script")
+            .find_all()
+            .filter_map(|s| match s.text() {
+                t if t.starts_with("window._rcState_") => Some(t),
+                _ => None,
+            })
+            .next();
+
+        let valeur = match script {
+            Some(s) => serde_json::from_str(&s)?,
+            None => return Err("script introuvable".into()),
+        };
     }
     let mut json = env::temp_dir();
     json.push(out);
-    fs::write(json, serde_json::to_string(&émissions)?)?;
-    
+    fs::write(json, serde_json::to_string(&épisodes)?)?;
+
     Ok(())
 }
 
