e20a52474fac59816659378863c9f3d4ff279912
Rrogntudju <rrogntudju@example.com>
2021-10-14 12:05:15 -0400

    wip

--- crates/gratte/src/main.rs
+++ crates/gratte/src/main.rs
@@ -1,9 +1,9 @@
-use std::error::Error;
-use std::default::Default;
 use serde::Serialize;
+use std::default::Default;
 use std::env;
-use std::io::{BufWriter, Write};
-use std::fs::File;
+use std::error::Error;
+use std::fs;
+use Soup;
 
 #[derive(Serialize)]
 struct Emission {
@@ -14,28 +14,30 @@ struct Emission {
 #[derive(Serialize, Default)]
 struct Emissions(Vec<Emission>);
 
-fn gratte(url: &str, out: &str) ->  Result<(), Box<dyn Error>> {
+fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
     let mut émissions = Emissions::default();
-    let mut json = env::temp_dir();
-    json.push(out);
-    let mut file = BufWriter::new(File::create(json)?);
     for i in 1.. {
         let response = match minreq::get(url).with_timeout(10).send() {
-            Ok(response) => {
-                match response.status_code {
-                    200 => response,
-                    403 => break,
-                    _ => return Err(format!("{} a retourné {}", url, response.reason_phrase).into())
-                }
-            }
+            Ok(response) => match response.status_code {
+                200 => response,
+                403 => break,
+                _ => return Err(format!("{} a retourné {}", url, response.reason_phrase).into()),
+            },
             Err(e) => return Err(e.into()),
         };
+        let soup = Soup::new(response);
     }
-    file.flush()?;
+    let mut json = env::temp_dir();
+    json.push(out);
+    fs::write(json, serde_json::to_string(&émissions)?)?;
+    
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    gratte("https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber={}", "csb.json")?;
+    gratte(
+        "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber={}",
+        "csb.json",
+    )?;
     Ok(())
 }
