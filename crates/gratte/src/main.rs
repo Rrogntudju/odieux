ffc8902f4db2f5c45b1b08852c5db9cd764b7078
Rrogntudju <rrogntudju@example.com>
2021-10-14 10:55:24 -0400

    wip

--- crates/gratte/src/main.rs
+++ crates/gratte/src/main.rs
@@ -1,20 +1,31 @@
 use std::error::Error;
 use std::default::Default;
 use serde::Serialize;
+use std::env;
+use std::io::BufWriter;
+use std::fs::File;
+
+#[derive(Serialize)]
+struct Emission {
+    titre: String,
+    url: String,
+}
 
 #[derive(Serialize, Default)]
-struct Emissions {
-    urls: Vec<String>,
-}
- 
-fn main() -> Result<(), Box<dyn Error>> {
-    
-    let url = "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber={}";
-    let émissions = Emissions::default();
+struct Emissions(Vec<Emission>);
 
+fn gratte(url: &str, out: &str) ->  Result<(), Box<dyn Error>> {
+    let émissions = Emissions::default();
+    let mut json = env::temp_dir();
+    json.push(out);
+    let mut file = BufWriter::new(File::create(json)?);
     for i in 1.. {
 
     }
+    Ok(())
+}
 
+fn main() -> Result<(), Box<dyn Error>> {
+    gratte("https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber={}", "csb.json")?;
     Ok(())
 }
