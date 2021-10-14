9576e1e952a148be31aafb37a93471a12efd1da6
Rrogntudju <rrogntudju@example.com>
2021-10-14 11:10:31 -0400

    wip

--- crates/gratte/src/main.rs
+++ crates/gratte/src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::default::Default;
 use serde::Serialize;
 use std::env;
-use std::io::BufWriter;
+use std::io::{BufWriter, Write};
 use std::fs::File;
 
 #[derive(Serialize)]
@@ -15,13 +15,23 @@ struct Emission {
 struct Emissions(Vec<Emission>);
 
 fn gratte(url: &str, out: &str) ->  Result<(), Box<dyn Error>> {
-    let émissions = Emissions::default();
+    let mut émissions = Emissions::default();
     let mut json = env::temp_dir();
     json.push(out);
     let mut file = BufWriter::new(File::create(json)?);
     for i in 1.. {
-
+        let response = match minreq::get(url).with_timeout(10).send() {
+            Ok(response) => {
+                match response.status_code {
+                    200 => response,
+                    403 => break,
+                    _ => return Err(format!("{} a retourné {}", url, response.reason_phrase).into())
+                }
+            }
+            Err(e) => return Err(e.into()),
+        };
     }
+    file.flush()?;
     Ok(())
 }
 
