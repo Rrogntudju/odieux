3d8cbb5ff15f5e389823d272d900708eedf7d36c
Rrogntudju <rrogntudju@example.com>
2021-10-15 12:11:14 -0400

    wip

--- crates/gratte/src/main.rs
+++ crates/gratte/src/main.rs
@@ -17,7 +17,7 @@ struct Episodes(Vec<Episode>);
 
 fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
     let mut épisodes = Episodes::default();
-    for i in 1.. {
+    for i in 1..2 {
         let url = format!("{}{}", url, i);
         let page = match minreq::get(&url).with_timeout(10).send() {
             Ok(response) => match response.status_code {
@@ -38,7 +38,7 @@ fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
             .next();
 
         let valeur: Value = match script {
-            Some(s) => serde_json::from_str(s.trim_start_matches("window._rcState_ = /*bns*/ "))?,
+            Some(s) => serde_json::from_str(s.trim_start_matches("window._rcState_ = /*bns*/ ").trim_end_matches(" /*bne*/;"))?,
             None => return Err("script introuvable".into()),
         };
         let items = &valeur["pagesV2"]["pages"][url.trim_start_matches("https://ici.radio-canada.ca")]["data"]["content"]["contentDetail"]["items"];
@@ -68,7 +68,7 @@ fn gratte(url: &str, out: &str) -> Result<(), Box<dyn Error>> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     gratte(
-        "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber={}",
+        "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber=",
         "csb.json",
     )?;
     Ok(())
