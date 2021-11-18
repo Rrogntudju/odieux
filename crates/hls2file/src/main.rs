f99b15052fdd3274ce82e0eeedbd1d14f9f9a43f
Rrogntudju <rrogntudju@example.com>
2021-11-18 18:42:22 -0500

    wip

--- crates/hls2file/src/main.rs
+++ crates/hls2file/src/main.rs
@@ -3,18 +3,34 @@ use std::env::args;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use gratte::gratte;
+
+const TIME_OUT: u64 = 10;
+const CSB: &str = "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber=";
+const URL_VALIDEUR: &str = "https://services.radio-canada.ca/media/validation/v2/?appCode=medianet&connectionType=hd&deviceType=ipad&idMedia={}&multibitrate=true&output=json&tech=hls";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let master_url = match args().nth(1) {
+    let page = match args().nth(1) {
         Some(arg) => arg,
-        None => return Err("Fournir un url master.m3u8 validé".into()),
+        None => return Err("Fournir le numéro de la page".into()),
     };
 
-    let aac_filename = match args().nth(2) {
+    let numéro = match args().nth(2) {
+        Some(arg) => arg,
+        None => return Err("Fournir le numéro de l'émission".into()),
+    };
+
+    let aac_filename = match args().nth(3) {
         Some(arg) => arg,
         None => return Err("Fournir un nom de fichier aac".into()),
     };
 
+    let émissions = gratte(CSB, page.parse::<usize>()?)?;
+    let url = URL_VALIDEUR.replace("{}", &émissions[numéro.parse::<usize>()? + 1].media_id);
+    let value = minreq::get(&url)
+        .with_timeout(TIME_OUT)
+        .send()?
+        .json()?;
     let rx = hls_handler::start(&master_url)?;
     let mut aac = env::temp_dir();
     aac.push(aac_filename);
