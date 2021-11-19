b37ca25181cd837f30fcec1764d0638a7e2e4e7f
Rrogntudju <rrogntudju@example.com>
2021-11-19 15:08:18 -0500

    Pour tester un fichier aac

--- crates/hplay/src/main.rs
+++ crates/hplay/src/main.rs
@@ -1,14 +1,20 @@
+use rodio::{decoder::Decoder, OutputStream, Sink};
 use std::env::args;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let arg = match args().nth(1) {
         Some(arg) => arg,
-        None => return Err("Fournir un url master.m3u8 validé".into()),
+        None => return Err("Fournir le chemin du fichier aac".into()),
     };
 
-    let (player, _output_stream) = hls_player::start(&arg)?;
-    player.sleep_until_end();
+    let (_output_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+    let source = Decoder::new(BufReader::new(File::open(arg)?))?;
+    sink.append(source);
+    sink.sleep_until_end();
 
     Ok(())
 }
