a2a74a75de0225ed66993ec76279010e5cdf244d
Rrogntudju <rrogntudju@example.com>
2021-12-19 19:02:58 -0500

    oubli

--- crates/hls_play_live/src/main.rs
+++ crates/hls_play_live/src/main.rs
@@ -1,11 +1,16 @@
 use serde_json::value::Value;
 use std::error::Error;
+use std::time::Duration;
+use reqwest::Client;
 
 const TIME_OUT: u64 = 10;
 const URL_VALIDEUR: &str = "https://services.radio-canada.ca/media/validation/v2/?appCode=medianetlive&connectionType=hd&deviceType=ipad&idMedia=cbvx&multibitrate=true&output=json&tech=hls";
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let value: Value = minreq::get(URL_VALIDEUR).with_timeout(TIME_OUT).send()?.json()?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::builder().timeout(Duration::from_secs(TIME_OUT)).build()?;
+    let response = client.get(URL_VALIDEUR).send().await?.text().await?;
+    let value: Value = serde_json::from_str(&response)?;
     let (sink, _output_stream) = hls_player::start(value["url"].as_str().unwrap_or_default())?;
     sink.sleep_until_end();
 
