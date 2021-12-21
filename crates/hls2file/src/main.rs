6c80d6b3790b2d4458042ae57d480c1e259b0238
Rrogntudju <rrogntudju@example.com>
2021-12-21 14:13:39 -0500

    wip

--- crates/hls2file/src/main.rs
+++ crates/hls2file/src/main.rs
@@ -10,35 +10,40 @@ use tokio::io::{AsyncWriteExt, BufWriter};
 
 const TIME_OUT: u64 = 10;
 const CSB: &str = "https://ici.radio-canada.ca/ohdio/musique/emissions/1161/cestsibon?pageNumber=";
-const URL_VALIDEUR: &str = "https://services.radio-canada.ca/media/validation/v2/?appCode=medianet&connectionType=hd&deviceType=ipad&idMedia={}&multibitrate=true&output=json&tech=hls";
+const URL_VALIDEUR_OD: &str = "https://services.radio-canada.ca/media/validation/v2/?appCode=medianet&connectionType=hd&deviceType=ipad&idMedia={}&multibitrate=true&output=json&tech=hls";
+const URL_VALIDEUR_LIVE: &str = "https://services.radio-canada.ca/media/validation/v2/?appCode=medianetlive&connectionType=hd&deviceType=ipad&idMedia=cbvx&multibitrate=true&output=json&tech=hls";
 const PAGES: usize = 68;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let page = match args().nth(1) {
-        Some(arg) => arg,
-        None => return Err("Fournir le numéro de la page".into()),
-    };
-
-    let num = match args().nth(2) {
-        Some(arg) => arg,
-        None => return Err("Fournir le numéro de l'épisode".into()),
-    };
-
-    let page = page.parse::<usize>()?.clamp(1, PAGES);
     let client = Client::builder().timeout(Duration::from_secs(TIME_OUT)).build()?;
-    let épisodes = gratte(CSB, page, &client).await?;
+    let (url, titre) = if args().len() > 1 {
+        let page = match args().nth(1) {
+            Some(arg) => arg,
+            None => return Err("Fournir le numéro de la page".into()),
+        };
 
-    let num = num.parse::<usize>()?.clamp(1, épisodes.len());
-    let media_id = &épisodes[num - 1].media_id;
-    if media_id.is_empty() {
-        return Err("Aucune musique diffusée disponible".into());
-    }
-    let url = URL_VALIDEUR.replace("{}", media_id);
+        let num = match args().nth(2) {
+            Some(arg) => arg,
+            None => return Err("Fournir le numéro de l'épisode".into()),
+        };
+
+        let page = page.parse::<usize>()?.clamp(1, PAGES);
+        let épisodes = gratte(CSB, page, &client).await?;
+
+        let num = num.parse::<usize>()?.clamp(1, épisodes.len());
+        let media_id = &épisodes[num - 1].media_id;
+        if media_id.is_empty() {
+            return Err("Aucune musique diffusée disponible".into());
+        }
+        (URL_VALIDEUR_OD.replace("{}", media_id), épisodes[num - 1].titre.clone())
+    } else {
+        (URL_VALIDEUR_LIVE.to_owned(), "direct".to_owned())
+    };
     let handle = tokio::spawn(async move { client.get(&url).send().await?.text().await });
 
     let mut aac = env::temp_dir();
-    aac.set_file_name(&épisodes[num - 1].titre);
+    aac.set_file_name(&titre);
     aac.set_extension("aac");
     let mut file = BufWriter::new(File::create(aac).await?);
 
