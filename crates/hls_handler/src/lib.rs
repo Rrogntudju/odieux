80b395cea42640b3326734c64461ea894e794d68
Rrogntudju <rrogntudju@example.com>
2022-11-22 12:21:16 -0500

    tant qu'à y être

--- crates/hls_handler/src/lib.rs
+++ crates/hls_handler/src/lib.rs
@@ -19,7 +19,7 @@ enum InitState {
 type Message = Result<Vec<u8>>;
 
 const TIME_OUT: u64 = 30;
-const MAX_RETRIES: usize = 10;
+const MAX_RETRIES: usize = 20;
 const RETRY_DELAY: u64 = 250;
 const BOUND: usize = 3;
 
