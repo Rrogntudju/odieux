985791283da6fd7b7b5834a402c08203ef87450f
Rrogntudju <rrogntudju@example.com>
2025-05-22 11:06:41 -0400

    wip

--- crates/hls_play_od/src/main.rs
+++ crates/hls_play_od/src/main.rs
@@ -1,4 +1,4 @@
-use media::get_episodes;
+use media::{get_episodes, get_media_id};
 use reqwest::Client;
 use serde_json::Value;
 use std::env::args;
@@ -10,32 +10,28 @@ const URL_VALIDEUR: &str = "https://services.radio-canada.ca/media/validation/v2
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let erreur = "Args: <url du programme> <page> <épisode>";
+    let erreur = "Args: <id du programme> <page> <no de l'épisode>";
     let mut args = args();
     let prog = match args.nth(1) {
-        Some(arg) => arg,
+        Some(arg) => arg.parse::<usize>().unwrap_or_default(),
         None => return Err(erreur.into()),
     };
 
     let page = match args.next() {
+        Some(arg) => arg.parse::<usize>().unwrap_or_default(),
+        None => return Err(erreur.into()),
+    };
+
+    let épisode_no = match args.next() {
         Some(arg) => arg,
         None => return Err(erreur.into()),
     };
 
-    let num = match args.next() {
-        Some(arg) => arg,
-        None => return Err(erreur.into()),
-    };
+    let épisodes = get_episodes(prog, page).await?;
+    let no = épisode_no.parse::<usize>()?.clamp(1, épisodes.len());
+    let media_id = get_media_id(&épisodes[no].id).await?;
 
-    let page = page.parse::<usize>()?;
-    let épisodes = get_episodes(page, &prog).await?;
-
-    let num = num.parse::<usize>()?.clamp(1, épisodes.len());
-    let media_id = &épisodes[num - 1].media_id;
-    if media_id.is_empty() {
-        return Err("Aucune musique diffusée disponible".into());
-    }
-    let url = URL_VALIDEUR.replace("{}", media_id);
+    let url = URL_VALIDEUR.replace("{}", &media_id);
     let client = Client::builder().timeout(Duration::from_secs(TIME_OUT)).build()?;
     let response = client.get(&url).send().await?.text().await?;
     let value: Value = serde_json::from_str(&response)?;
